4990856c74f15401727735655c85782068292905
JI-EUN <pangdoa_merry@daum.net>
2023-06-02 11:53:29 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Blog</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">글보기</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container mt-4">
+    <h2>블로그임</h2>
+    <p>아무튼 블로그임</p>
+  </div>
+
+  <List v-for="(lists,i) in blogList" :key="i" :list="lists" /> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import blog from './assets/data/blogData'
+import List from './components/BlogList';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      blogList: blog
+    }
+  },  
   components: {
-    HelloWorld
+    List
   }
 }
 </script>
@@ -21,6 +49,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/BlogList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h5>{{ list.title}}</h5>
+        <p>{{ list.date }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name:'BlogList',
+    props:{
+        list : Object
+    }
+}
+</script>
+<style>
+</style>
